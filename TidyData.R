057a0f3e595abdd89ad9e3f49064072d96f072a2
Richard Lent <richardlent@gmail.com>
2019-06-13 15:27:57 -0400

    TidyData.R once again is able to access the shared Google Sheet.

--- TidyData.R
+++ TidyData.R
@@ -5,29 +5,43 @@
 library(pheatmap) # Pretty heatmaps.
 library(tidyr)
 library(googledrive)
+library(googlesheets)
 library(googlesheets4)
 library(dplyr)
 
+# This variable eventually will be picked off an input list
+# in the Shiny app.
 x <- "Week" # The quantitative variable for the heatmap.
 
 # Get the shared Google Sheet and read it into an R data frame.
-sheets_auth(email = "rlent@holycross.edu") # Probably should encrypt this.
-stem <- drive_get("STEM Topics Data", team_drive = "Integrated Science/Neuroscience") # A dribble.
-stemData <- read_sheet(stem$id)     # A tibble.
-stemData <- as.data.frame(stemData) # Convert back to data frame so we can have row names.
+# sheets_auth(email = "rlent@holycross.edu") # Probably should encrypt this.
+
+stemData <-
+  drive_get("STEM Topics Data", team_drive = "Integrated Science/Neuroscience") %>%
+  select(id) %>%
+  combine() %>%
+  gs_key(lookup = FALSE,
+         visibility = "private") %>%
+  gs_read_csv()
+
+# stem <- drive_get("STEM Topics Data", team_drive = "Integrated Science/Neuroscience") # A dribble.
+# stemData <- read_sheet(stem)        # A tibble.
+# stemData <- as.data.frame(stemData) # Convert back to data frame so we can have row names.
+
+# None of the following will work until the data source stabilizes.
 
 # Make the data matrix needed by pheatmap.
-theData <- select(stemData, 'Topic Name', Course, x)
-theData <- spread(theData, Course, x)
-rownames(theData) <- theData$'Topic Name'
-theData$'Topic Name' <- NULL
-theData <- as.matrix(theData)
+# theData <- select(stemData, 'Topic Name', Course, x)
+# theData <- spread(theData, Course, x)
+# rownames(theData) <- theData$'Topic Name'
+# theData$'Topic Name' <- NULL
+# theData <- as.matrix(theData)
 
 # Can adjust margins of the heatmap by changing cellheight and cellwidth.
-pheatmap(theData, legend = FALSE,
-         display_numbers = TRUE, number_format = "%i", fontsize_number = 25,
-         cellheight = 75, cellwidth = 150,
-         main = paste0("Heatmap of ", x, " by Course and Topic\n"))
+# pheatmap(theData, legend = FALSE,
+#          display_numbers = TRUE, number_format = "%i", fontsize_number = 25,
+#          cellheight = 75, cellwidth = 150,
+#          main = paste0("Heatmap of ", x, " by Course and Topic\n"))
 
 
 
