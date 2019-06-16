1071b676e8f9647c89b127027b4a49ea5ac46eb8
Richard Lent <richardlent@gmail.com>
2019-06-16 17:16:25 -0400

    Update heatmap.R
    Apparently we can edit and commit code on CodeHub app.

--- heatmap.R
+++ heatmap.R
@@ -3,6 +3,8 @@
 # Uses CSV file data1.csv.
 # Richard A. Lent, June 2019.
 
+# Editing this on CodeHub app. Then what?
+
 library(pheatmap) # Pretty heatmaps.
 
 data1 <- as.matrix(read.csv("data1.csv", header = TRUE))
