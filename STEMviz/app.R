4fdb5d0586359797fa999892383a79ed3066e313
Richard Lent <richardlent@gmail.com>
2019-06-17 16:52:42 -0400

    Shiny app is now displaying the data table and a heatmap.

--- STEMviz/app.R
+++ STEMviz/app.R
@@ -5,11 +5,18 @@ library(shiny)
 library(googledrive)
 library(googlesheets)
 library(dplyr)
+library(pheatmap)
+library(tidyr)
+
+# This variable eventually will be picked off an input list
+# in the Shiny app.
+x <- "Week" # The quantitative variable for the heatmap.
 
 ui <- fluidPage(
     
     titlePanel("STEMviz"),
-    dataTableOutput("theSheet")
+    dataTableOutput("theSheet"),
+    plotOutput("theHeatmap")
 
 ) # ui
 
@@ -23,8 +30,24 @@ server <- function(input, output) {
                visibility = "private") %>%
         gs_read_csv()
     
+    # Make the data matrix needed by pheatmap.
+    stemData <- as.data.frame(stemData) # Convert back to data frame so we can have row names.
+    theData <- select(stemData, 'Topic Name', Course, x)
+    theData <- spread(theData, Course, x)
+    rownames(theData) <- theData$'Topic Name'
+    theData$'Topic Name' <- NULL
+    theData <- as.matrix(theData)
+    
     output$theSheet <- renderDataTable({
-        stemData
+        theData
+    })
+    
+    output$theHeatmap <- renderPlot({
+        # Can adjust margins of the heatmap by changing cellheight and cellwidth.
+        pheatmap(theData, legend = FALSE,
+                 display_numbers = TRUE, number_format = "%i", fontsize_number = 25,
+                 cellheight = 75, cellwidth = 150,
+                 main = paste0("Heatmap of ", x, " by Course and Topic\n"))
     })
 
 } # server
