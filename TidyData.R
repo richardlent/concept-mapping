a4afb292dc60a9d0bffd5e5b64a0c375f4eac5a0
Richard Lent <richardlent@gmail.com>
2019-06-17 16:33:36 -0400

    TidyData.R is now using the test Google Sheet called `Test Please Ignore`.

--- TidyData.R
+++ TidyData.R
@@ -16,31 +16,26 @@ x <- "Week" # The quantitative variable for the heatmap.
 # sheets_auth(email = "rlent@holycross.edu") # Probably should encrypt this.
 
 stemData <-
-  drive_get("STEM Topics Data", team_drive = "Integrated Science/Neuroscience") %>%
+  drive_get("Test Please Ignore", team_drive = "Integrated Science/Neuroscience") %>%
   select(id) %>%
   combine() %>%
   gs_key(lookup = FALSE,
          visibility = "private") %>%
   gs_read_csv()
 
-# stem <- drive_get("STEM Topics Data", team_drive = "Integrated Science/Neuroscience") # A dribble.
-# stemData <- read_sheet(stem)        # A tibble.
-# stemData <- as.data.frame(stemData) # Convert back to data frame so we can have row names.
-
-# None of the following will work until the data source stabilizes.
-
 # Make the data matrix needed by pheatmap.
-# theData <- select(stemData, 'Topic Name', Course, x)
-# theData <- spread(theData, Course, x)
-# rownames(theData) <- theData$'Topic Name'
-# theData$'Topic Name' <- NULL
-# theData <- as.matrix(theData)
+stemData <- as.data.frame(stemData) # Convert back to data frame so we can have row names.
+theData <- select(stemData, 'Topic Name', Course, x)
+theData <- spread(theData, Course, x)
+rownames(theData) <- theData$'Topic Name'
+theData$'Topic Name' <- NULL
+theData <- as.matrix(theData)
 
 # Can adjust margins of the heatmap by changing cellheight and cellwidth.
-# pheatmap(theData, legend = FALSE,
-#          display_numbers = TRUE, number_format = "%i", fontsize_number = 25,
-#          cellheight = 75, cellwidth = 150,
-#          main = paste0("Heatmap of ", x, " by Course and Topic\n"))
+pheatmap(theData, legend = FALSE,
+         display_numbers = TRUE, number_format = "%i", fontsize_number = 25,
+         cellheight = 75, cellwidth = 150,
+         main = paste0("Heatmap of ", x, " by Course and Topic\n"))
 
 
 
