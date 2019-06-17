d9cc08f7fea64469ba601a4d989503c60d903583
Richard Lent <richardlent@gmail.com>
2019-06-17 12:38:14 -0400

    Adding folder for STEMviz Shiny app.

---
+++ STEMviz/app.R
@@ -0,0 +1,36 @@
+# STEMviz, a Shiny app for visualization of STEM topics and concept mapping.
+# Richard Lent, Thursday, June 13, 2019.
+
+library(shiny)
+library(googledrive)
+library(googlesheets)
+
+gs_auth(token = "shiny_app_token.rds")
+
+ui <- fluidPage(
+    
+    titlePanel("STEMviz"),
+    dataTableOutput("theSheet")
+
+) # ui
+
+server <- function(input, output) {
+    
+    stemData <-
+        drive_get("STEM Topics Data", team_drive = "Integrated Science/Neuroscience") %>%
+        select(id) %>%
+        combine() %>%
+        gs_key(lookup = FALSE,
+               visibility = "private") %>%
+        gs_read_csv()
+    
+    output$theSheet <- renderDataTable({
+        stemData
+    })
+
+} # server
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
+
