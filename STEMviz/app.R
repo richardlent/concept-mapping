58eef889b9cf03a8f09a26511914422a978a23a4
Richard Lent <richardlent@gmail.com>
2019-06-18 12:38:31 -0400

    STEMviz now has basic functionality.

--- STEMviz/app.R
+++ STEMviz/app.R
@@ -9,13 +9,15 @@ library(pheatmap)
 library(tidyr)
 library(DT)
 
-# This variable eventually will be picked off an input list
-# in the Shiny app.
-x <- "Introduced/Reinforced" # The quantitative variable for the heatmap.
-
 ui <- fluidPage(
     
     titlePanel("STEMviz"),
+    p(),
+    selectInput(
+        "theVariable",
+        "Select the variable to map",
+        c("Week", "Early/Middle/Late", "Introduced/Reinforced")
+    ),
     DT::dataTableOutput("theSheet"),
     plotOutput("theHeatmap")
 
@@ -31,24 +33,22 @@ server <- function(input, output) {
                visibility = "private") %>%
         gs_read_csv()
     
-    # Make the data matrix needed by pheatmap.
-    stemData <- as.data.frame(stemData) # Convert back to data frame so we can have row names.
-    theData <- select(stemData, 'Topic Name', Course, x)
-    theData <- spread(theData, Course, x)
-    rownames(theData) <- theData$'Topic Name'
-    theData$'Topic Name' <- NULL
-    theData <- as.matrix(theData)
-    
-    output$theSheet <- DT::renderDataTable({
-        datatable(theData)
-    })
-    
     output$theHeatmap <- renderPlot({
+        # Make the data matrix needed by pheatmap.
+        stemData <- as.data.frame(stemData) # Convert back to data frame so we can have row names.
+        theData <- select(stemData, 'Topic Name', Course, input$theVariable)
+        theData <- spread(theData, Course, input$theVariable)
+        rownames(theData) <- theData$'Topic Name'
+        theData$'Topic Name' <- NULL
+        theData <- as.matrix(theData)
+        output$theSheet <- DT::renderDataTable({
+            datatable(theData)
+        })
         # Can adjust margins of the heatmap by changing cellheight and cellwidth.
         pheatmap(theData, legend = FALSE,
                  display_numbers = TRUE, number_format = "%i", fontsize_number = 25,
                  cellheight = 75, cellwidth = 150,
-                 main = paste0("Heatmap of ", x, " by Course and Topic\n"))
+                 main = paste0("Heatmap of ", input$theVariable, " by Course and Topic\n"))
     })
 
 } # server
