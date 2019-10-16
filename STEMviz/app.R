4350849f1c858f44601e2cf42ba464ed2e819c8e
Richard Lent <richardlent@gmail.com>
2019-10-16 14:00:03 -0400

    Housekeeping prior to converting the app to a split-file app.

--- STEMviz/app.R
+++ STEMviz/app.R
@@ -9,15 +9,18 @@ library(pheatmap)
 library(tidyr)
 library(DT)
 library(readr)
+library(superheat)
 
 helpTxt <- read_lines("help.txt")
 
+drive_auth(email = "rlent@holycross.edu")
+
 stemData <-
-    drive_get("Test Please Ignore", team_drive = "Integrated Science/Neuroscience") %>%
+    # drive_get("Test Please Ignore", team_drive = "Integrated Science/Neuroscience") %>%
+    drive_get("STEMunit1", team_drive = "Integrated Science/Neuroscience") %>%
     select(id) %>%
     combine() %>%
-    gs_key(lookup = FALSE,
-           visibility = "private") %>%
+    gs_key(lookup = FALSE, visibility = "private") %>%
     gs_read_csv()
 
 ui <- fluidPage(
@@ -40,22 +43,24 @@ server <- function(input, output) {
     output$theHeatmap <- renderPlot({ # To make room for title. May have to tweak later.
         # Make the data matrix needed by pheatmap.
         stemData <- as.data.frame(stemData) # Convert back to data frame so we can have row names.
-        theData <- select(stemData, 'Topic Name', Course, input$theVariable)
+        stemData <- mutate(stemData, 'TopicCourse' = paste0(stemData$`Topic Name`, '-' , stemData$Course))
+        theData <- select(stemData, 'TopicCourse', Course, input$theVariable)
         theData <- spread(theData, Course, input$theVariable)
-        rownames(theData) <- theData$'Topic Name'
-        theData$'Topic Name' <- NULL
+        rownames(theData) <- theData$'TopicCourse'
+        theData$'TopicCourse' <- NULL
         theData <- as.matrix(theData)
         output$theSheet <- DT::renderDataTable({
             # datatable(theData)
             datatable(stemData)
         })
         # Can adjust margins of the heatmap by changing cellheight and cellwidth.
-        pheatmap(theData, legend = FALSE,
-                 display_numbers = TRUE, number_format = "%i", fontsize_number = 25,
-                 cellheight = 75, cellwidth = 150,
-                 fontsize_row = 16, fontsize_col = 16, fontsize = 12,
-                 main = paste0("Heatmap of ", input$theVariable, " by Course and Topic\n"))
-    })
+        # pheatmap(theData, legend = FALSE,
+        #          display_numbers = TRUE, number_format = "%i", fontsize_number = 25,
+        #          cellheight = 75, cellwidth = 150,
+        #          fontsize_row = 16, fontsize_col = 16, fontsize = 12,
+        #          main = paste0("Heatmap of ", input$theVariable, " by Course and Topic\n"))
+        superheat(theData)
+    }, height = 2000)
     
     observeEvent(input$apphelp, {
         helpContent <- HTML(helpTxt)
